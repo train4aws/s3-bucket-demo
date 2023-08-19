f6b2488ef3849774fcd817b77c12694151d94e98
Idris Fagbemi <idrisniyi94@gmail.com>
2023-08-18 23:50:09 -0500

    Destroying resources

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-resource "aws_s3_bucket" "this" {
-  bucket = "idris-s3-bucket-demo-2023099"
-  tags = {
-    Name = "s3-bucket-demo-2023"
-  }
-}
+# resource "aws_s3_bucket" "this" {
+#   bucket = "idris-s3-bucket-demo-2023099"
+#   tags = {
+#     Name = "s3-bucket-demo-2023"
+#   }
+# }
