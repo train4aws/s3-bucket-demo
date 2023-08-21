1be8bb279cec7dde73b22d39a0c7f3ff021bc2f8
train4aws <79434869+train4aws@users.noreply.github.com>
2023-08-20 23:41:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 # resource "aws_s3_bucket" "this" {
-#   bucket = "idris-s3-bucket-demo-2023099"
+#   bucket = "train4aws-s3-bucket-demo-2023099"
 #   tags = {
 #     Name = "s3-bucket-demo-2023-${var.environment}"
 #   }
